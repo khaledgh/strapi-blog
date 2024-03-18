691646d5d76657d780a222f427da4bd7a514d1ae
Khaled Ghourani <Khaledghourani4@gmail.com>
2024-03-19 01:15:06 +0300

    last update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost', 'blog.bracketed.tech'],
-      },
+  images: {
+    domains: ["localhost", "blog.bracketed.tech"],
+  },
+  publicRuntimeConfig: {
+    trailingSlash: true,
+  },
 };
 
 export default nextConfig;
