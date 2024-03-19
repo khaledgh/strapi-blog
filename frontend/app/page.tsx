cc21f98c38d90be2e2ef1e91427c9e15a8fdcd4a
Khaled Ghourani <Khaledghourani4@gmail.com>
2024-03-19 17:48:22 +0300

    dynamic

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,15 @@
-"use client";
-import { useEffect, useState } from "react";
 import ArticlesList from "./_components/ArticlesList";
 import GlobalApi from "./_utils/GlobalApi";
-import { AxiosResponse } from "axios";
 
-export const dynamic = "force-dynamic";
-export default function Home() {
-  const [articles, SetArticles] = useState<Article[]>([]);
-  useEffect(() => {
-    getArticleList();
-  }, []);
-  const getArticleList = () => {
-    GlobalApi.getArticlesList().then(
-      (resp: AxiosResponse<ArticlesResponse>) => {
-        SetArticles(resp?.data?.data);
-      }
-    );
-  };
+async function getData() {
+  const resp = await GlobalApi.getArticlesList();
+  return resp?.data?.data;
+}
+
+export const dynamic = 'force-dynamic';
+
+export default async function Home() {
+  const articles = await getData();
   return (
     <main className="flex min-h-screen flex-col py-10 px-24">
       <div>
