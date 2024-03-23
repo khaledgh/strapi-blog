551b2ee73c43412378888332c1ba9cdb4b4a58c4
Khaled Ghourani <Khaledghourani4@gmail.com>
2024-03-23 14:37:53 +0300

    dynamic

--- frontend/app/_components/CodeText.tsx
+++ frontend/app/_components/CodeText.tsx
@@ -1,13 +1,12 @@
 "use client"
-import React, { useState } from "react";
+import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 // import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
  import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 // import { tomorrowNight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 //import { monokai } from 'react-syntax-highlighter/dist/esm/styles/prism';
 //import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
- 
-import { IconCheck, IconCopy } from "@tabler/icons-react";
+import { CopyBlock, vs2015} from "react-code-blocks"; 
 
 interface HighlightedTextProps {
   html: string;
@@ -31,57 +30,15 @@ const HighlightedText: React.FC<HighlightedTextProps> = ({ html }) => {
       const key = `REACT_SYNTAX_HIGHLIGHTER_${currentIndex++}`;
 
       highlightedCodeMap[key] = (
-        <div
-          style={{
-            position: "relative",
-            borderRadius: "1px",
-            overflow: "hidden",
-          }}
-        >
-          <SyntaxHighlighter
-            language={language}
-            style={atomDark}
-            customStyle={{
-              scrollbarWidth: "thin", // Firefox
-              scrollbarColor: "rgba(59, 130, 246, 0.3) transparent", // Firefox
-            }}
-          >
-            {decodeEntities(code.trim())}
-          </SyntaxHighlighter>
-          <div
-            style={{
-              position: "absolute",
-              top: "0",
-              right: "0",
-              padding: "15px",
-              background: "transparent",
-              border: "none",
-              cursor: "pointer",
-            }}
-          >
-            {/* <CopyButton value={decodeEntities(code.trim())} timeout={3000}>
-              {({ copied, copy }) => (
-                <Tooltip
-                  label={copied ? "Copied" : "Copy"}
-                  withArrow
-                  position="right"
-                >
-                  <ActionIcon
-                    color={copied ? "teal" : "gray"}
-                    variant="subtle"
-                    onClick={copy}
-                  >
-                    {copied ? (
-                      <IconCheck style={{ width: 18 }} />
-                    ) : (
-                      <IconCopy style={{ width: 18 }} />
-                    )}
-                  </ActionIcon>
-                </Tooltip>
-              )}
-            </CopyButton> */}
-          </div>
-        </div>
+        <CopyBlock 
+              text={decodeEntities(code)}
+              language={language}
+              showLineNumbers={false}
+              theme={vs2015} 
+              copied={false}
+              wrapLongLines
+            /> 
+
       );
 
       return "";
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
 const nextConfig = {
   images: {
     domains: ["localhost", "blog.bracketed.tech"],
   },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
@@ -12,4 +15,13 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
